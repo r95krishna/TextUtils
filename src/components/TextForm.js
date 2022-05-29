c2fc6de289d3be048ede531a77ca8befb9a48342
Rahul Krishna <rahulkrishna@Rahuls-MacBook-Air.local>
2022-05-29 22:26:59 +0530

    fix for minutes read function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,11 @@ export default function TextForm(props) {
         setText(event.target.value)
      }
 
+     const clearTextHandle = () =>{
+        let newtext=""
+        setText(newtext)
+     }
+
     const UpperCaseClick=()=>{
          console.log("upper case clicked "+ text)
          let newUperCaseText= text.toUpperCase();
@@ -20,21 +25,35 @@ export default function TextForm(props) {
         setText(newLowerCaseText)
     }
 
-    const [text,setText] = useState("Enter text here");
+    const CapitalizeTextHandle=()=>{
+    
+
+    }
+
+    const [text,setText] = useState("Enter text here...");
     //setText("rahul")
   return (
     <>
         <div className="mb-3">
-            <div>
+            <div className='mb-3'>
                 <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
-                <textarea className="form-control" id="mybox" value= {text} onChange={onChangeMethod} rows="8"></textarea>
+                <textarea className="form-control" id="mybox" value= {text} onChange={onChangeMethod} onClick={clearTextHandle} rows="8"></textarea>
             </div>
-            
-                <button type="button" className="btn btn-outline-primary" onClick={UpperCaseClick}>Convert To UpperCase</button>
+                <button type="button" className="btn btn-outline-primary mx-2" onClick={UpperCaseClick}>Convert To UpperCase</button>
+                <button type="button" className="btn btn-outline-primary mx-2" onClick={LowerCaseCLick}>Convert To LowerCase</button> 
+                <button type="button" className="btn btn-outline-primary mx-2" onClick={clearTextHandle}>Clear Text</button> 
+                <button type="button" className="btn btn-outline-primary mx-2" onClick={CapitalizeTextHandle}>Capitalize Text</button> 
 
-                <button type="button" className="btn btn-outline-primary" onClick={LowerCaseCLick}>Convert To LowerCase</button>
-            
         </div>
+        <div className="container">
+            <h1> Text Summary </h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{text.length === 0 ? 0 : 0.008 * text.split(" ").length.toFixed(1)} minutes of read</p>
+            <h2> Preview </h2>
+            <p>{text}</p>
+        </div>
+
+        
     </>
   )
 }
