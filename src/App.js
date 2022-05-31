adbde743ac49ecca4c8e94f24a2354bf043347e3
Rahul Krishna <rahulkrishna@Rahuls-MacBook-Air.local>
2022-06-01 00:27:46 +0530

    enhanced dark mode

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import {useState} from 'react'
 
 function App() {
 
-  
+    const [mode, setMode] = useState("light")
+    const [toggleText, setToggleText] = useState("Enable Dark Mode")
+    const [text, setText] = useState('dark')
+
+    const toggleMode = () =>{
+        if(mode==='light'){
+          setMode('dark')
+          setText('light')
+          setToggleText("Enable Light Mode")
+
+        }
+        else{
+          setMode('light')
+          setText('dark')
+          setToggleText("Enable Dark Mode")
+        }
+    }
+
+
   return (
     <>
        {/* <Navbar title="Textutils" aboutext="aboutext" />   */}
-       <Navbar title="TitleUtils"/> 
+       <Navbar title="TitleUtils" mode={mode} toggleMode={toggleMode} toggleText={toggleText} text={text}/> 
        {/* <div className='container'><TextForm heading="Please enter your String"/></div>  */}
-       <About/>
+       {/* <About/> */}
        
     </>
   );
